9abbacdc485d13494c01a506b163cccacaa65d14
Keijiro Takahashi <keijiro@Ultra.jt.unity3d.com>
2016-03-15 17:28:56 +0900

    Initial commit.

---
+++ Assets/Test/DrawMeshTester.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class DrawMeshTester : MonoBehaviour
+{
+    [SerializeField] Mesh _mesh;
+    [SerializeField] Material _material;
+
+    MaterialPropertyBlock _block;
+
+    void OnEnable()
+    {
+        _block = new MaterialPropertyBlock();
+    }
+
+    void OnDisable()
+    {
+        _block = null;
+    }
+
+    void Update()
+    {
+        LightProbeUtility.SetSHCoefficients(transform.position, _block);
+
+        Graphics.DrawMesh(
+            _mesh,
+            transform.position, transform.rotation,
+            _material, 0, null, 0, _block
+        );
+    }
+}
